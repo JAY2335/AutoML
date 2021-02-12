bc174c6c3065485d074ef4ab38dffcd9c165248f
Yuthirshta Prasad T <yuthish661@gmail.com>
2021-02-13 02:03:54 +0530

    FIX:Preprocessing function bug fixed

--- API/Preprocess/Preprocessing.py
+++ API/Preprocess/Preprocessing.py
@@ -4,56 +4,69 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 def null_checking(dataset):
   null_columns=[ i for i in dataset if dataset[f"{i}"].isnull().sum()>0 ]
-  return True if null_columns == [] else False
+  if (null_columns == []): 
+    return False
+  return True 
 
 def null_processing(dataset, option):
+  
   try:
     
     null_columns=[ i for i in dataset if dataset[f"{i}"].isnull().sum()>0 ]
     object_col=[i for i in null_columns if dataset[f"{i}"].dtype=='object']
-  
+     
   except Exception as e:
     return -1,str(e) + '[Error During Encoding]',dataset
   
   try:
     for i in object_col:
       dataset[f"{i}"].fillna(method='ffill',inplace=True)
+      dataset[f"{i}"].fillna(method='bfill',inplace=True)
+    
   except Exception as e:
     return -1,str(e) + '[Error During Encoding]',dataset
 
   try:
+    X=dataset.iloc[:,:-1]
+    y=pd.DataFrame(dataset['target'])
+    if (dataset['target'].dtype!='object'):
+      y.fillna(method='ffill',inplace=True)
+      y.fillna(method='bfill',inplace=True)
     if (option=="mean"):
-      dataset.fillna(dataset.mean(),inplace=True)
+      X.fillna(dataset.mean(),inplace=True)
     if (option=="median"):
-      dataset.fillna(dataset.median(),inplace=True)
+      X.fillna(dataset.median(),inplace=True)
     if (option=="mode"):
-      dataset.fillna(dataset.mode(),inplace=True)
+      X.fillna(dataset.median(),inplace=True)
+    result=pd.concat([X,y],axis=1)
 
   except Exception as e:
     return -1,str(e) + '[Error During Filling]',dataset
 
-  return 0,'DONE',dataset
-
+  return 0,'DONE',result
 
 def encoding(dataset):
+  
   try:
     le=LabelEncoder()
   
     if dataset['target'].dtype=='object':
       y=dataset['target'].values
       y=le.fit_transform(y)
-    else:
-      y=dataset['target'].values
   except Exception as e:
     return -1,str(e) + '[Error During Encoding]',dataset
   
   try:
     #dataset.drop(['target'],inplace=True,axis=1)
-    dataset=pd.get_dummies(dataset,columns=None,drop_first=True)
+    X=dataset.iloc[:,:-1]
+    X=pd.get_dummies(X,columns=None,drop_first=True)
     #X=dataset.iloc[:,:].values
+    y=pd.DataFrame(dataset['target'])
+    result=pd.concat([X,y],axis=1)
+    
   except Exception as e:
     return -1,str(e) + '[Error During Encoding]',dataset
   
-  return 0,'DONE PREPROCESSING',dataset
+  return 0,'DONE PREPROCESSING',result
 
 
